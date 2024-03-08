df7988b8935e8a1e58f0267b9a0a458dfd2c118e
ShaLan <aqero@msn.cn>
2024-03-08 17:48:30 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,16 +3,16 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--no-gradio-queu
 
 @REM Uncomment following code to reference an existing A1111 checkout.
-@REM set A1111_HOME=Your A1111 checkout dir
+set A1111_HOME=E:/SD_CommonFiles
 @REM
-@REM set VENV_DIR=%A1111_HOME%/venv
-@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
-@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
-@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
-@REM  --embeddings-dir %A1111_HOME%/embeddings ^
-@REM  --lora-dir %A1111_HOME%/models/Lora
+set VENV_DIR=%A1111_HOME%/venv
+set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
+--ckpt-dir %A1111_HOME%/Stable-diffusion ^
+--hypernetwork-dir %A1111_HOME%/hypernetworks ^
+--embeddings-dir %A1111_HOME%/embeddings ^
+--lora-dir %A1111_HOME%/Lora
 
 call webui.bat
